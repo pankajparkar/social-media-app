7aad6f1558e2583f5b7160749daef99692bf4016
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-01-14 02:39:15 +0530

    Server side rendering sample ready page.
    Isomorphic code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  data;
+  constructor(private httpClient: HttpClient){}
+  getPost(){
+    this.httpClient.get('https://jsonplaceholder.typicode.com/posts/1').subscribe(
+      (data) => this.data = data
+    )
+  }
+  ngOnInit(){
+    this.getPost();
+  }
 }
